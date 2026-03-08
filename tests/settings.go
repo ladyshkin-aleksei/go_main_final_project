6855098c4c6ab57b03b6722e49fa14b4c9a397ab
ladyshkin-aleksei <ladyshkin2000@mail.ru>
2026-03-08 15:36:20 +0300

    fifth commit

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = false
 var Search = false
-var Token = ``
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZF9oYXNoIjoiZWNkNzE4NzBkMTk2MzMxNmE5N2UzYWMzNDA4Yzk4MzVhZDhjZjBmM2MxYmM3MDM1MjdjMzAyNjU1MzRmNzVhZSIsImV4cCI6MTc3MzAwMjAxOH0.OpDXMGoR-wDNppJ4A0Yg6RP0OvH5aloRQur0t-E4MqI`
